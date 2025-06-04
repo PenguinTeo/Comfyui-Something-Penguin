2a7527e59a4e61a2cf84493154dfc7d305ff0f70
Penguin <yihanzhang7678@gmail.com>
2025-06-04 23:25:47 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,10 +6,20 @@ from .color_nodes import (
     PerspectiveTransformNode,
 )
 
-__all__ = [
-    "BrightnessContrastNode",
-    "LevelsNode",
-    "HueSaturationNode",
-    "ColorBalanceNode",
-    "PerspectiveTransformNode",
-]
+NODE_CLASS_MAPPINGS = {
+    "BrightnessContrastNode": BrightnessContrastNode,
+    "LevelsNode": LevelsNode,
+    "HueSaturationNode": HueSaturationNode,
+    "ColorBalanceNode": ColorBalanceNode,
+    "PerspectiveTransformNode": PerspectiveTransformNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "BrightnessContrastNode": "Brightness/Contrast",
+    "LevelsNode": "Levels",
+    "HueSaturationNode": "Hue/Saturation",
+    "ColorBalanceNode": "Color Balance",
+    "PerspectiveTransformNode": "Perspective Transform",
+}
+
+__all__ = list(NODE_CLASS_MAPPINGS.keys())
